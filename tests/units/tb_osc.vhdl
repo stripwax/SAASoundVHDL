26d175fb31d12caf5fadae217159d05029b905a1
Dave Hooper <dave@beermex.com>
2025-10-12 00:08:50 +0100

    more comprehensive osc test including case for 'expected' glitch test

--- tests/units/tb_osc.vhdl
+++ tests/units/tb_osc.vhdl
@@ -309,120 +309,31 @@ begin
         end loop;
     end loop;
 
-    wait;
-
     -- test changing frequency ; should take effect only on the next half wave
     -- continue where we left off (above) but change frequency after a few cycles (123)
     for i in 1 to 123 loop
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '0'; -- question, should this be 0 or 1??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '0'; -- question, should this be 0 or 1??
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '0'; -- question, should this be 0 or 1??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '0'; -- question, should this be 0 or 1??
+        for c in 1 to cycles loop
+            clk <= '1';
+            wait for 125 ns;
+            assert output = '0'; -- question, should this be 0 or 1??
+            clk <= '0';
+            wait for 125 ns;
+            assert output = '0'; -- question, should this be 0 or 1??
+        end loop;
     end loop;
     frequency <= "11111111";
     for i in 1 to 389 loop  -- rest of THIS half-wave unchanged
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '0'; -- question, should this be 0 or 1??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '0'; -- question, should this be 0 or 1??
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '0'; -- question, should this be 0 or 1??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '0'; -- question, should this be 0 or 1??
-    end loop;
-    for i in 1 to 512 loop -- octave register was not written so NEXT half-wave also unchanged (freq therefore deferred to the FOLLOWING half-wave)
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
-    end loop;
-    for i in 1 to 257 loop  -- after that previous half-wave finished, freq reloaded and now takes effect:
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '0' report "oh" & INTEGER'IMAGE(i); -- question, should this be 1 or 0??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '0'; -- question, should this be 1 or 0??
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '0'; -- question, should this be 1 or 0??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '0'; -- question, should this be 1 or 0??
-    end loop;
-
-    -- test changing it back, but that might even be a redundant test
-    for i in 1 to 123 loop
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
-    end loop;
-    frequency <= "10101010";  -- 170 ;  so half-wave expected to be (512-170) = 342
-    for i in 1 to 134 loop  -- 257 minus 123
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
-    end loop;
-    for i in 1 to 257 loop  -- still old freq since octave not written
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '0' report "oh" & INTEGER'IMAGE(i); -- question, should this be 1 or 0??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '0'; -- question, should this be 1 or 0??
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '0'; -- question, should this be 1 or 0??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '0'; -- question, should this be 1 or 0??
-    end loop;    
-    for j in 1 to 5 loop
-        for i in 1 to 342 loop
+        for c in 1 to cycles loop
             clk <= '1';
             wait for 125 ns;
-            assert output = '1'; -- question, should this be 1 or 0??
+            assert output = '0'; -- question, should this be 0 or 1??
             clk <= '0';
             wait for 125 ns;
-            assert output = '1'; -- question, should this be 1 or 0??
+            assert output = '0'; -- question, should this be 0 or 1??
+        end loop;
+    end loop;
+    for i in 1 to 512 loop -- octave register was not written so NEXT half-wave also unchanged (freq therefore deferred to the FOLLOWING half-wave)
+        for c in 1 to cycles loop
             clk <= '1';
             wait for 125 ns;
             assert output = '1'; -- question, should this be 1 or 0??
@@ -430,84 +341,253 @@ begin
             wait for 125 ns;
             assert output = '1'; -- question, should this be 1 or 0??
         end loop;
+    end loop;
+    for i in 1 to 257 loop  -- after that previous half-wave finished, freq reloaded and now takes effect:
+        for c in 1 to cycles loop
+            clk <= '1';
+            wait for 125 ns;
+            assert output = '0' report "oh" & INTEGER'IMAGE(i); -- question, should this be 1 or 0??
+            clk <= '0';
+            wait for 125 ns;
+            assert output = '0'; -- question, should this be 1 or 0??
+        end loop;
+    end loop;
+
+    -- test changing it back, but that might even be a redundant test
+    for i in 1 to 123 loop
+        for c in 1 to cycles loop
+            clk <= '1';
+            wait for 125 ns;
+            assert output = '1'; -- question, should this be 1 or 0??
+            clk <= '0';
+            wait for 125 ns;
+            assert output = '1'; -- question, should this be 1 or 0??
+        end loop;
+    end loop;
+    frequency <= "10101010";  -- 170 ;  so half-wave expected to be (512-170) = 342
+    for i in 1 to 134 loop  -- 257 minus 123
+        for c in 1 to cycles loop
+            clk <= '1';
+            wait for 125 ns;
+            assert output = '1'; -- question, should this be 1 or 0??
+            clk <= '0';
+            wait for 125 ns;
+            assert output = '1'; -- question, should this be 1 or 0??
+        end loop;
+    end loop;
+    for i in 1 to 257 loop  -- still old freq since octave not written
+        for c in 1 to cycles loop
+            clk <= '1';
+            wait for 125 ns;
+            assert output = '0' report "oh" & INTEGER'IMAGE(i); -- question, should this be 1 or 0??
+            clk <= '0';
+            wait for 125 ns;
+            assert output = '0'; -- question, should this be 1 or 0??
+        end loop;
+    end loop;    
+    for j in 1 to 5 loop
         for i in 1 to 342 loop
-            clk <= '1';
-            wait for 125 ns;
-            assert output = '0'; -- question, should this be 1 or 0??
-            clk <= '0';
-            wait for 125 ns;
-            assert output = '0'; -- question, should this be 1 or 0??
-            clk <= '1';
-            wait for 125 ns;
-            assert output = '0'; -- question, should this be 1 or 0??
-            clk <= '0';
-            wait for 125 ns;
-            assert output = '0'; -- question, should this be 1 or 0??
+            for c in 1 to cycles loop
+                clk <= '1';
+                wait for 125 ns;
+                assert output = '1'; -- question, should this be 1 or 0??
+                clk <= '0';
+                wait for 125 ns;
+                assert output = '1'; -- question, should this be 1 or 0??
+            end loop;
+        end loop;
+        for i in 1 to 342 loop
+            for c in 1 to cycles loop
+                clk <= '1';
+                wait for 125 ns;
+                assert output = '0'; -- question, should this be 1 or 0??
+                clk <= '0';
+                wait for 125 ns;
+                assert output = '0'; -- question, should this be 1 or 0??
+            end loop;
         end loop;
     end loop;
 
     -- now test the "freq and octave latch" behaviour i.e. glitch-free frequency change
     -- When you set octave, it snaps the frequency reg too, and sets them both on the next half-wave
+    -- we want to check the cases where they are set simultaneously (although impossible on real device)
+    -- and sequentially (realistically 8 clocks minimum would separate those changes but we'll also test with 1 clock separation)
     for i in 1 to 123 loop
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
+        for c in 1 to cycles loop
+            clk <= '1';
+            wait for 125 ns;
+            assert output = '1'; -- question, should this be 1 or 0??
+            clk <= '0';
+            wait for 125 ns;
+            assert output = '1'; -- question, should this be 1 or 0??
+        end loop;
     end loop;
-    frequency <= "11110000";  -- 240 , so half-wave expected to be (512-240) = 272
+    frequency <= "11110000";  -- 240 , so NEXT half-wave expected to be (512-240) = 272
     octave <= "100";
     octave_wr <= '1';
     for i in 1 to 219 loop -- 342 minus 123
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '1' report "oh" & INTEGER'IMAGE(i); -- question, should this be 1 or 0??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
-        octave_wr <= '0';
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
+        for c in 1 to cycles loop
+            clk <= '1';
+            wait for 125 ns;
+            assert output = '1' report "oh" & INTEGER'IMAGE(i); -- question, should this be 1 or 0??
+            clk <= '0';
+            wait for 125 ns;
+            assert output = '1'; -- question, should this be 1 or 0??
+            octave_wr <= '0';
+        end loop;
+    end loop;
+    -- new octave (and frequency) kicks in here.  cycles set accordingly: octave 100=4 => 16 cycles @ 8mhz for octave counter
+    cycles := 16;
+    for i in 1 to 272 loop
+        for c in 1 to cycles loop
+            clk <= '1';
+            wait for 125 ns;
+            assert output = '0'; -- question, should this be 1 or 0??
+            clk <= '0';
+            wait for 125 ns;
+            assert output = '0'; -- question, should this be 1 or 0??
+        end loop;
     end loop;
     for i in 1 to 272 loop
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '0'; -- question, should this be 1 or 0??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '0'; -- question, should this be 1 or 0??
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '0'; -- question, should this be 1 or 0??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '0'; -- question, should this be 1 or 0??
+        for c in 1 to cycles loop
+            clk <= '1';
+            wait for 125 ns;
+            assert output = '1'; -- question, should this be 1 or 0??
+            clk <= '0';
+            wait for 125 ns;
+            assert output = '1'; -- question, should this be 1 or 0??
+        end loop;
     end loop;
-    for i in 1 to 272 loop
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
-        clk <= '1';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
-        clk <= '0';
-        wait for 125 ns;
-        assert output = '1'; -- question, should this be 1 or 0??
+
+    for i in 1 to 123 loop
+        for c in 1 to cycles loop
+            clk <= '1';
+            wait for 125 ns;
+            assert output = '0'; -- question, should this be 1 or 0??
+            clk <= '0';
+            wait for 125 ns;
+            assert output = '0'; -- question, should this be 1 or 0??
+        end loop;
     end loop;
+    frequency <= "11111110";  -- 254 , so NEXT half-wave expected to be (512-254) = 258
+    for i in 1 to 45 loop
+        for c in 1 to cycles loop
+            clk <= '1';
+            wait for 125 ns;
+            assert output = '0'; -- question, should this be 1 or 0??
+            clk <= '0';
+            wait for 125 ns;
+            assert output = '0'; -- question, should this be 1 or 0??
+        end loop;
+    end loop;
+    octave <= "010";
+    octave_wr <= '1';
+    for i in 1 to 104 loop  -- 272 - 123 - 45
+        for c in 1 to cycles loop
+            clk <= '1';
+            wait for 125 ns;
+            assert output = '0'; -- question, should this be 1 or 0??
+            clk <= '0';
+            wait for 125 ns;
+            assert output = '0'; -- question, should this be 1 or 0??
+            octave_wr <= '0';
+        end loop;
+    end loop;
+    cycles := 64;
+    for i in 1 to 258 loop
+        for c in 1 to cycles loop
+            clk <= '1';
+            wait for 125 ns;
+            assert output = '1'; -- question, should this be 1 or 0??
+            clk <= '0';
+            wait for 125 ns;
+            assert output = '1'; -- question, should this be 1 or 0??
+        end loop;
+    end loop;
+    for i in 1 to 258 loop
+        for c in 1 to cycles loop
+            clk <= '1';
+            wait for 125 ns;
+            assert output = '0'; -- question, should this be 1 or 0??
+            clk <= '0';
+            wait for 125 ns;
+            assert output = '0'; -- question, should this be 1 or 0??
+        end loop;
+    end loop;
+
+    -- add a test case for the 'glitch' case i.e. setting octave before frequency
+    -- the octave write snapshots the frequency, so you would get one half-cycle
+    -- at the new octave but old frequency, then he next half cycle has the new frequency
+    for i in 1 to 123 loop
+        for c in 1 to cycles loop
+            clk <= '1';
+            wait for 125 ns;
+            assert output = '1'; -- question, should this be 1 or 0??
+            clk <= '0';
+            wait for 125 ns;
+            assert output = '1'; -- question, should this be 1 or 0??
+        end loop;
+    end loop;
+    octave <= "001";
+    octave_wr <= '1';
+    for i in 1 to 45 loop
+        for c in 1 to cycles loop
+            clk <= '1';
+            wait for 125 ns;
+            assert output = '1'; -- question, should this be 1 or 0??
+            clk <= '0';
+            wait for 125 ns;
+            assert output = '1'; -- question, should this be 1 or 0??
+            octave_wr <= '0';
+        end loop;
+    end loop;
+    frequency <= "10011001";  -- 153 , so NEXT half-wave expected to be (512-153) = 359
+    for i in 1 to 90 loop -- 90 = 258 -123 - 45
+        for c in 1 to cycles loop
+            clk <= '1';
+            wait for 125 ns;
+            assert output = '1'; -- question, should this be 1 or 0??
+            clk <= '0';
+            wait for 125 ns;
+            assert output = '1'; -- question, should this be 1 or 0??
+            octave_wr <= '0';
+        end loop;
+    end loop;
+    -- octave kicks in (so, cycles now changes, but not frequency)
+    cycles := 128;
+    for i in 1 to 258 loop  -- 258 = old frequency
+        for c in 1 to cycles loop
+            clk <= '1';
+            wait for 125 ns;
+            assert output = '0'; -- question, should this be 1 or 0??
+            clk <= '0';
+            wait for 125 ns;
+            assert output = '0'; -- question, should this be 1 or 0??
+        end loop;
+    end loop;
+    -- frequency kicks in
+    for i in 1 to 359 loop  -- 359 = new frequency
+        for c in 1 to cycles loop
+            clk <= '1';
+            wait for 125 ns;
+            assert output = '1'; -- question, should this be 1 or 0??
+            clk <= '0';
+            wait for 125 ns;
+            assert output = '1'; -- question, should this be 1 or 0??
+        end loop;
+    end loop;
+    for i in 1 to 359 loop  -- 359 = new frequency
+        for c in 1 to cycles loop
+            clk <= '1';
+            wait for 125 ns;
+            assert output = '0'; -- question, should this be 1 or 0??
+            clk <= '0';
+            wait for 125 ns;
+            assert output = '0'; -- question, should this be 1 or 0??
+        end loop;
+    end loop;
+
+
 
     --  Wait forever; this will finish the simulation.
     wait;
