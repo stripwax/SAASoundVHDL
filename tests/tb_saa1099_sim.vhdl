dbbd8cc99b44d35216520e9e28e08d0f89c702b4
Dave Hooper <dave@beermex.com>
2025-10-18 00:23:16 +0100

    checkpoint

--- tests/tb_saa1099_sim.vhdl
+++ tests/tb_saa1099_sim.vhdl
@@ -63,7 +63,7 @@ procedure read(l : inout line; val : inout unsigned; ok : out boolean) is
     variable done : boolean := false;
     variable c_ok : boolean := true;
     variable retVal : unsigned(val'length - 1 downto 0) := (others => '0');
-    variable digit : natural;
+    variable something : boolean := false;
 begin
     while not done loop
         read(l, c, c_ok);
@@ -73,13 +73,18 @@ begin
             if (decodeDigit(c) < 0) then
                 done := true;
             else
+                something := true;
                 retVal := resize(retVal * to_unsigned(10, 4), val'length);
                 retVal := retVal + to_unsigned(decodeDigit(c), 4);
             end if;
         end if;
     end loop;
     val := retVal;
-    ok := c_ok;
+    if something then
+        ok := true;
+    else
+        ok := c_ok;
+    end if;
 end procedure read;
 
 begin
@@ -101,6 +106,8 @@ begin
     variable ok : boolean;
     variable char : character;
     --variable wait_time : time;
+    variable reg : unsigned(7 downto 0);
+    variable data_byte : unsigned(7 downto 0);
     variable sample_pos : unsigned(31 downto 0);
     variable the_word_ns : string(2 downto 1);
     variable selector : bit_vector(1 downto 0);
@@ -125,9 +132,6 @@ begin
             report "Read 'sample_pos' failed for line: " & text_line.all
             severity failure;
 
-            write(OUTPUT, "hi " & to_hstring(sample_pos) & LF);
-            write(OUTPUT, "hi " & to_hstring(ns_per_sample) & LF);
-            write(OUTPUT, "hi " & to_hstring(sample_ns) & LF);
             sample_ns := resize(sample_pos * ns_per_sample, sample_ns'length);
 
             if sample_ns > current_ns then
@@ -136,54 +140,45 @@ begin
                     wait for 125 ns;
                     clk <= '0';
                     wait for 125 ns;
-                    current_ns := current_ns + 250;
+                    current_ns := current_ns + to_unsigned(250, 8);
                 end loop;
             end if;
-            
-            read(text_line, char, ok); -- Skip expected space which would otherwise appear during string read
-            read(text_line, the_word_ns, ok);
+
+            read(text_line, reg, ok);
             assert ok
-            report "Read 'the_word_ns' failed for line: " & text_line.all
+            report "Read 'reg' failed for line: " & text_line.all
             severity failure;
 
-            hread(text_line, selector, ok);
-            assert ok
-            report "Read 'sel' failed for line: " & text_line.all
-            severity failure;
-            --sel <= selector;
-            
-            hread(text_line, data, ok);
-            assert ok
-            report "Read 'din_0' failed for line: " & text_line.all
-            severity failure;
-            --din_0 <= data;
-            
-            hread(text_line, data, ok);
-            assert ok
-            report "Read 'din_1' failed for line: " & text_line.all
-            severity failure;
-            --din_1 <= data;
-            
-            hread(text_line, data, ok);
-            assert ok
-            report "Read 'din_2' failed for line: " & text_line.all
-            severity failure;
-            --din_2 <= data;
-            
-            hread(text_line, data, ok);
-            assert ok
-            report "Read 'din_3' failed for line: " & text_line.all
-            severity failure;
-            --din_3 <= data;
-
-            -- Print trailing comment to console, if any
-            read(text_line, char, ok); -- Skip expected newline
-            read(text_line, char, ok);
-            if char = '#' then
-            read(text_line, char, ok); -- Skip expected newline
-            report text_line.all;
+            read(text_line, data_byte, ok);
+            if not ok then
+                -- just a reg write
+                write(OUTPUT, "REG " & to_hstring(reg) & LF);
+                a0 <= '1';
+                wr_n <= '0';
+                cs_n <= '0';
+                d <= std_logic_vector(reg);
+                clk <= '1';
+                wait for 125 ns;
+                clk <= '0';
+                wait for 125 ns;
+                wr_n <= '1';
+                cs_n <= '1';
+                current_ns := current_ns + to_unsigned(250, 8);
+            else
+                write(OUTPUT, "DATA: (REG" & to_hstring(reg) & ") = " & to_hstring(data_byte) & LF);
+                a0 <= '0';
+                wr_n <= '0';
+                cs_n <= '0';
+                d <= std_logic_vector(data_byte);
+                clk <= '1';
+                wait for 125 ns;
+                clk <= '0';
+                wait for 125 ns;
+                wr_n <= '1';
+                cs_n <= '1';
+                current_ns := current_ns + to_unsigned(250, 8);
             end if;
-        
+
         end loop;
         
         finish;
